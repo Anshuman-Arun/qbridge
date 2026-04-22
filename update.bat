4023a5a17fef7f81fc6f163d784afde5e59fd8c7
Anshuman Arun <anshumanarun2008@gmail.com>
2026-04-21 23:20:47 -0700

    update

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
-robocopy "C:\Users\anshu\Documents\Coding\qbridge" "C:\Users\anshu\Documents\GitHub\qbridge" /MIR /XD .git
+robocopy "C:\Users\anshu\Documents\Coding\qbridge" "C:\Users\anshu\Documents\GitHub\qbridge" /MIR /XD .git node_modules .next .open-next
 git -C "C:\Users\anshu\Documents\GitHub\qbridge" add .
 git -C "C:\Users\anshu\Documents\GitHub\qbridge" commit -m "update"
 git -C "C:\Users\anshu\Documents\GitHub\qbridge" push origin main
