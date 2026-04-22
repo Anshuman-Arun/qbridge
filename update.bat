47dddfa6d677c94c31f906d54a048878c6bc6a54
Anshuman Arun <anshumanarun2008@gmail.com>
2026-04-21 22:56:44 -0700

    update

--- update.bat
+++ update.bat
@@ -2,6 +2,6 @@
 robocopy "C:\Users\anshu\Documents\Coding\qbridge" "C:\Users\anshu\Documents\GitHub\qbridge" /MIR /XD .git
 git -C "C:\Users\anshu\Documents\GitHub\qbridge" add .
 git -C "C:\Users\anshu\Documents\GitHub\qbridge" commit -m "update"
-git -C "C:\Users\anshu\Documents\GitHub\qbridge" push
+git -C "C:\Users\anshu\Documents\GitHub\qbridge" push origin main
 echo Done!
 pause
